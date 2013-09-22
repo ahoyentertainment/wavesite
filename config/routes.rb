7da72e139f0594a826104ef489e37cda6ee24c06
Zach Easley <amissmarkoloca3@gmail.com>
2013-09-21 20:04:38 -0500

    Added layout code and user model and controller. Need options for bday. Plans for user settings

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Wavesite::Application.routes.draw do
+  get "users/new"
+  root  'pages#home'
+  match '/help', to: 'pages#help', via: 'get'
+  match '/about', to: 'pages#about', via: 'get'
+  match '/contact', to: 'pages#contact', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
